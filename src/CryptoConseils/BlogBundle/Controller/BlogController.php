492913e18ded3f37cc8771122945b9d2c479c6a1
ValentinLENFANT <valentin.lenfant@hotmail.com>
2018-03-13 11:34:33 +0100

    Add the main routes for Blog Bundle

--- src/CryptoConseils/BlogBundle/Controller/BlogController.php
+++ src/CryptoConseils/BlogBundle/Controller/BlogController.php
@@ -10,6 +10,7 @@ namespace CryptoConseils\BlogBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class BlogController extends Controller
 {
@@ -17,5 +18,26 @@ class BlogController extends Controller
     {
         $content = $this->get('templating')->render('CryptoConseilsBlogBundle:Blog:index.html.twig', array('nom' => 'Valentin'));
         return new Response($content);
+    }
+
+    public function viewAction($id)
+    {
+        $url = $this->get('router')->generate('cryptoconseils_blog_view', array('id' => $id), UrlGeneratorInterface::ABSOLUTE_URL);
+        return new Response("Affichage de l'article d'id : " .$id. " d'URL " .$url);
+    }
+
+    public function addAction()
+    {
+        //todo
+    }
+
+    public function editAction($id)
+    {
+        //todo
+    }
+
+    public function deleteAction($id)
+    {
+        //todo
     }
 }
