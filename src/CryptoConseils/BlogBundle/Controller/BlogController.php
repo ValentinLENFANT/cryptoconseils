1ee1f70bbfe8a46372cdf34da02ed0d28aef0800
ValentinLENFANT <valentin.lenfant@hotmail.com>
2018-04-02 17:28:35 +0200

    Created the CRUD's views for BlogBundle

--- src/CryptoConseils/BlogBundle/Controller/BlogController.php
+++ src/CryptoConseils/BlogBundle/Controller/BlogController.php
@@ -14,23 +14,29 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class BlogController extends Controller
 {
-    public function indexAction($page)
+    public function indexAction()
     {
-        if($page <1) {
-           throw new NotFoundHttpException('La page ' .$page. ' est inexistante');
-        }
-
-        return $this->render('CryptoConseilsBlogBundle:Blog:index.html.twig');
+        return $this->render('CryptoConseilsBlogBundle:Blog:index.html.twig', array('listAdverts' => array()));
     }
 
     public function viewAction($id)
     {
-        return $this->render('CryptoConseilsBlogBundle:Blog:view.html.twig', array('id' => $id));
+        $article = array(
+            'title' => 'Est-ce que le Bitcoin est toujours roi ?',
+            'id' => $id,
+            'author' => 'Valentin LENFANT',
+            'content' => 'Le Bitcoin a vu sa dominance passer de 90% à 45%, soit une diminution de 50%. Est-il toujours le roi des crypto-monnaies ?',
+            'date' => new \Datetime()
+        );
+
+        return $this->render('CryptoConseilsBlogBundle:Blog:view.html.twig', array(
+            'article' => $article
+        ));
     }
 
     public function addAction(Request $request)
     {
-        if($request->isMethod('POST')) {
+        if ($request->isMethod('POST')) {
             $request->getSession()->getFlashBag()->add('notice', 'Article crée');
             return $this->redirectToRoute('cryptoconseils_blog_view', array('id' => 1));
         }
@@ -39,15 +45,38 @@ class BlogController extends Controller
 
     public function editAction($id, Request $request)
     {
-        if($request->isMethod('POST')) {
+        if ($request->isMethod('POST')) {
             $request->getSession()->getFlashBag()->add('notice', 'Article modifié');
             return $this->redirectToRoute('cryptoconseils_blog_view', array('id' => 5));
         }
-        return $this->render('CryptoConseilsBlogBundle:Blog:edit.html.twig');
+        $article = array(
+            'title' => 'Est-ce que le Bitcoin est toujours roi ?',
+            'id' => $id,
+            'author' => 'Valentin LENFANT',
+            'content' => 'Le Bitcoin a vu sa dominance passer de 90% à 45%, soit une diminution de 50%. Est-il toujours le roi des crypto-monnaies ?',
+            'date' => new \Datetime()
+        );
+
+        return $this->render('CryptoConseilsBlogBundle:Blog:edit.html.twig', array(
+            'article' => $article
+        ));
     }
 
     public function deleteAction($id)
     {
         return $this->render('CryptoConseilsBlogBundle:Blog:delete.html.twig');
+    }
+
+    public function menuAction($limit)
+    {
+        $listArticles = array(
+            array('id' => 2, 'title' => 'Est-ce que le Bitcoin est toujours roi ?'),
+            array('id' => 5, 'title' => 'Est-ce bientôt la fin du bear market ?'),
+            array('id' => 9, 'title' => 'Quel avenir pour les crypto-monnaies ?')
+        );
+
+        return $this->render('CryptoConseilsBlogBundle:Blog:menu.html.twig', array(
+            'listArticles' => $listArticles
+        ));
     }
 }
