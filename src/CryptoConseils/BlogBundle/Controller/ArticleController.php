181464a1ced51e6db6eb0485738b8ee5191e9779
ValentinLENFANT <valentin.lenfant@hotmail.com>
2018-04-10 16:24:00 +0200

    Use entities and repositories for articles

--- src/CryptoConseils/BlogBundle/Controller/ArticleController.php
+++ src/CryptoConseils/BlogBundle/Controller/ArticleController.php
@@ -11,6 +11,7 @@ namespace CryptoConseils\BlogBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use CryptoConseils\BlogBundle\Entity\Article;
 
 class ArticleController extends Controller
 {
@@ -21,13 +22,12 @@ class ArticleController extends Controller
 
     public function viewAction($id)
     {
-        $article = array(
-            'title' => 'Est-ce que le Bitcoin est toujours roi ?',
-            'id' => $id,
-            'author' => 'Valentin LENFANT',
-            'content' => 'Le Bitcoin a vu sa dominance passer de 90% à 45%, soit une diminution de 50%. Est-il toujours le roi des crypto-monnaies ?',
-            'date' => new \Datetime()
-        );
+        $repository = $this->getDoctrine()->getManager()->getRepository('CryptoConseilsBlogBundle:Article');
+        $article = $repository->find($id);
+
+        if (null === $article) {
+            throw new NotFoundHttpException("L'article d'id" .$id. "n'existe pas");
+        }
 
         return $this->render('CryptoConseilsBlogBundle:Article:view.html.twig', array(
             'article' => $article
@@ -36,11 +36,25 @@ class ArticleController extends Controller
 
     public function addAction(Request $request)
     {
+        // Création de l'entité
+        $article = new Article();
+        $article->setTitle('Est-ce que le Bitcoin est toujours roi ?');
+        $article->setAuthor('Valentin');
+        $article->setContent('Le Bitcoin a vu sa dominance passer de 90% à 45%, soit une diminution de 50%. Est-il toujours le roi des crypto-monnaies ?');
+
+        // Récupération de l'EntityManager
+        $em = $this->getDoctrine()->getManager();
+        // On persiste l'entité
+        $em->persist($article);
+        // Flush des persistence précédentes
+        $em->flush();
+
+
         if ($request->isMethod('POST')) {
             $request->getSession()->getFlashBag()->add('notice', 'Article crée');
-            return $this->redirectToRoute('cryptoconseils_blog_view', array('id' => 1));
+            return $this->redirectToRoute('cryptoconseils_blog_view', array($article => getId()));
         }
-        return $this->render('CryptoConseilsBlogBundle:Article:add.html.twig');
+        return $this->render('CryptoConseilsBlogBundle:Article:add.html.twig', array('article' => $article));
     }
 
     public function editAction($id, Request $request)
