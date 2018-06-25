7475bcb38e2e9b3bfba94bf023b236aef105de9d
Barbe-Bleue <screy@icloud.com>
2018-06-25 21:25:19 +0200

    add begin trade component

---
+++ cryptoconseils-frontend/src/Home/BeginTrade.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+class BeginTrade extends Component {
+  render() {
+    if(this.props.isLogged === false){
+      return (
+        <div className="BeginTrade Component">
+          <div className="call-action-all">
+            <div className="call-action-all-overlay">
+              <div className="container">
+                <div className="row">
+                  <div className="col-xs-12">
+                    {/* Call To Action Text Starts */}
+                    <div className="action-text">
+                      <h2>Commencez dès maintenant à trader</h2>
+                      <p className="lead">Créer un compte gratuitement !</p>
+                    </div>
+                    {/* Call To Action Text Ends */}
+                    {/* Call To Action Button Starts */}
+                    <p className="action-btn">
+                      <a className="btn btn-primary" href="/signup">
+                      S'enregistrer
+                      </a>
+                    </p>
+                    {/* Call To Action Button Ends */}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    }else {
+      return null;
+    }
+  }
+}
+
+export default BeginTrade;
