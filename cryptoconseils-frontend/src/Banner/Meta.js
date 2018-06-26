0372351082c80cea091937bb3473deea29862e87
Barbe-Bleue <screy@icloud.com>
2018-06-26 19:52:30 +0200

    create Meta component

---
+++ cryptoconseils-frontend/src/Banner/Meta.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+
+class Meta extends Component {
+  render() {
+    if(this.props.article) {
+      return(
+        <div className="Meta Component">
+          {/* Meta Starts */}
+          <div className="meta">
+            <span>
+              <i className="fa fa-user"></i> <a href="">{this.props.article.author}</a>
+            </span>
+            <span className="date">
+              <i className="fa fa-calendar"></i>{this.props.article.date}
+              </span>
+            <span>
+              <i className="fa fa-commenting"></i> <a href="blog-post.html">{this.props.coms}</a>
+            </span>
+            <span>
+              <i className="fa fa-tags"></i>{this.props.article.categories}
+            </span>
+          </div>
+          {/* Meta Ends */}
+        </div>
+      );
+    } else {
+      return null;
+    }
+  }
+}
+
+export default Meta;
