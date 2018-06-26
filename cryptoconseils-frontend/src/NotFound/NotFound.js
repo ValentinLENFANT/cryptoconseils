8b1adaf79c13da0859aae0ff6322068e5d5fe8b2
Barbe-Bleue <screy@icloud.com>
2018-06-26 19:50:53 +0200

    update 404

--- cryptoconseils-frontend/src/NotFound/NotFound.js
+++ cryptoconseils-frontend/src/NotFound/NotFound.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
-import is404 from './404-error.jpg';
 
 class NotFound extends Component {
   render() {
     return(
       <div align="center">
-        <img src={is404} alt="The not found image is not found... lol"/>
+        <img src="./404-error.jpg" alt="The not found image is not found... lol"/>
           <p>Vous pouvez retourner à l'accueil en cliquant <a href="/"> ici </a></p>
       </div>
     );
