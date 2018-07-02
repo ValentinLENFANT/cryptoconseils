54dbb4a2e9667474f75290f442a80d1a0b351532
Barbe-Bleue <screy@icloud.com>
2018-07-02 15:43:27 +0200

    display Denied component if not loged

--- cryptoconseils-frontend/src/Profil/Profil.js
+++ cryptoconseils-frontend/src/Profil/Profil.js
@@ -8,62 +8,70 @@ import Activites from './Activites';
 import Call from './Call';
 import Airdrop from './Airdrop';
 import InfosPerso from './InfosPerso';
-import UserAction from '../Home/UserAction'
+import UserAction from '../Home/UserAction';
+import Denied from '../Denied/Denied';
 
 class Profil extends Component {
   render() {
-    return (
-      <div className="App">
-      {/* Wrapper Starts */}
-        <div className="wrapper">
-          {/* Header Starts */}
-          <div className="header">
-            <div className="container">
-              <div className="row">
-                {/* Logo Starts */}
-                  <Logo />
-                {/* Logo Ends */}
-                {/* Statistics Starts */}
-                  <Statistics />
-                {/* Statistics Ends */}
-                {/* User Sign In/Sign Up Starts */}
-                  <UserAction />
-                {/* User Sign In/Sign Up Ends */}
+    if(sessionStorage.getItem('access_token')){
+      return (
+        <div className="App">
+        {/* Wrapper Starts */}
+          <div className="wrapper">
+            {/* Header Starts */}
+            <div className="header">
+              <div className="container">
+                <div className="row">
+                  {/* Logo Starts */}
+                    <Logo />
+                  {/* Logo Ends */}
+                  {/* Statistics Starts */}
+                    <Statistics />
+                  {/* Statistics Ends */}
+                  {/* User Sign In/Sign Up Starts */}
+                    <UserAction />
+                  {/* User Sign In/Sign Up Ends */}
+                </div>
               </div>
-            </div>
-            {/* Navigation Menu Starts */}
-              <Navigation />
-            {/* Navigation Menu Ends */}
-            </div>
+              {/* Navigation Menu Starts */}
+                <Navigation />
+              {/* Navigation Menu Ends */}
+              </div>
 
-          {/* Menus Starts */}
-            <Menus />
-          {/* Menus Ends */}
+            {/* Menus Starts */}
+              <Menus />
+            {/* Menus Ends */}
 
-          {/* Derniers commentaires Starts */}
-            <Activites />
-          {/* Derniers commentaires Ends */}
+            {/* Derniers commentaires Starts */}
+              <Activites />
+            {/* Derniers commentaires Ends */}
 
-          {/*Call premium Starts */}
-            <Call />
-          {/*Call premiums Ends */}
+            {/*Call premium Starts */}
+              <Call />
+            {/*Call premiums Ends */}
 
-          {/*Airdrop premium Starts */}
-          <Airdrop />
-          {/*Airdrop premiums Ends */}
+            {/*Airdrop premium Starts */}
+            <Airdrop />
+            {/*Airdrop premiums Ends */}
 
-          {/*InfosPerso Starts */}
-          <InfosPerso />
-          {/*InfosPerso Ends */}
+            {/*InfosPerso Starts */}
+            <InfosPerso />
+            {/*InfosPerso Ends */}
 
-          {/* Pricing Starts */}
-            <Price />
-          {/* Pricing Ends */}
+            {/* Pricing Starts */}
+              <Price />
+            {/* Pricing Ends */}
 
-        {/* Wrapper Ends */}
+          {/* Wrapper Ends */}
+          </div>
         </div>
-      </div>
-    );
+      );
+    } else {
+      return(
+        <Denied />
+      );
+    }
+
   }
 }
 
