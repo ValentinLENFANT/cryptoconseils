053ac4d9862ab7ff95d5bd0ca02cb13b849c349c
Barbe-Bleue <screy@icloud.com>
2018-07-02 20:36:16 +0200

    header in com

--- cryptoconseils-frontend/src/index.js
+++ cryptoconseils-frontend/src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+//import Header from './Header/Header'
 import Footer from './Footer/Footer';
 
 import registerServiceWorker from './registerServiceWorker';
 require('dotenv').config()
+
+/*ReactDOM.render(<Header />, document.getElementById('header'));
+registerServiceWorker();*/
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
