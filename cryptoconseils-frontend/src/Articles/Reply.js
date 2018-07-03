d9705c7f5aee683d6aeea4b18748fe73461635af
Barbe-Bleue <screy@icloud.com>
2018-07-03 23:37:45 +0200

    if no logged -> no comment input

--- cryptoconseils-frontend/src/Articles/Reply.js
+++ cryptoconseils-frontend/src/Articles/Reply.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios'
+import UserAction from '../Header/UserAction';
 
 class Reply extends Component {
 
@@ -24,18 +25,17 @@ class Reply extends Component {
   sendComment(event){
     // pour éviter le rechargement de la page
     event.preventDefault();
-
-    // check longeur du commentaire
-    if(this.state.comment.length < 30 || this.state.comment.length > 3000){
-      this.setState({
-        msg: "Votre commentaire doit contenir entre 30 et 3000 caractères"
-      })
-    } else {
+    if(sessionStorage.getItem('access_token')) {
       // check si access token
-      if(sessionStorage.getItem('access_token')){
-        var authorization = {
-          headers: {'Authorization': "Bearer " + sessionStorage.getItem('access_token')}
-        };
+      var authorization = {
+        headers: {'Authorization': "Bearer " + sessionStorage.getItem('access_token')}
+      };
+      // check longeur du commentaire
+      if(this.state.comment.length < 30 || this.state.comment.length > 3000){
+        this.setState({
+          msg: "Votre commentaire doit contenir entre 30 et 3000 caractères"
+        })
+      } else {
         // send coms
         axios.post(process.env.REACT_APP_API_ADDRESS+'/comments/new/', {
           article_id: this.props.article.id,
@@ -54,44 +54,57 @@ class Reply extends Component {
           this.setState({msg: "Une erreur s'est produite !"})
           console.log(error.response);
         });
-      } else {
-        // Si pas connecté
-        this.setState({
-          msg: "Vous devez être connecté pour écrire un commentaire"
-        })
       }
+    } else {
+      // Si pas connecté
+      this.setState({
+        msg: "Vous devez être connecté pour écrire un commentaire"
+      })
     }
   }
   render() {
-    return(
-      <div className="Reply Commponent">
-        <h3 className="comments-heading add-comment">Ajouter un commentaire</h3>
-        <p>{this.state.msg}</p>
-        {/* Comments Form Starts */}
-        <div className="comments-form">
-          <form onSubmit={this.sendComment}>
-            {/* Input Field Starts */}
-            <div className="form-group">
-              <textarea
-                className="form-control"
-                id="comment"
-                name="comment"
-                placeholder="COMMENT"
-                onChange={this.handleChange}
-                value={this.state.comment}
-                required>
-              </textarea>
-            </div>
-            {/* Input Field Ends */}
-            {/* Submit Form Button Starts */}
-            <div className="form-group">
-              <button className="btn btn-primary" type="submit" >Envoyer commentaire</button>
-            </div>
-            {/* Submit Form Button Ends */}
-          </form>
+    if(sessionStorage.getItem('access_token')) {
+      return(
+        <div className="Reply Commponent">
+          <h3 className="comments-heading add-comment">Ajouter un commentaire</h3>
+          <p>{this.state.msg}</p>
+          {/* Comments Form Starts */}
+          <div className="comments-form">
+            <form onSubmit={this.sendComment}>
+              {/* Input Field Starts */}
+              <div className="form-group">
+                <textarea
+                  className="form-control"
+                  id="comment"
+                  name="comment"
+                  placeholder="COMMENT"
+                  onChange={this.handleChange}
+                  value={this.state.comment}
+                  required>
+                </textarea>
+              </div>
+              {/* Input Field Ends */}
+              {/* Submit Form Button Starts */}
+              <div className="form-group">
+                <button className="btn btn-primary" type="submit" >Envoyer commentaire</button>
+              </div>
+              {/* Submit Form Button Ends */}
+            </form>
+          </div>
         </div>
-      </div>
-    );
+      );
+    } else {
+      return(
+        <div className="Reply Commponent">
+          <h3 className="comments-heading add-comment">
+            <span>Pour ajouter un commentaire Il faut vous</span>
+            <a href="/signin"> connecter </a>
+            <span>ou vous</span>
+            <a href="/signup>"> inscrire </a>
+          </h3>
+        </div>
+      );
+    }
   }
 }
 
