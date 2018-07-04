ca86395c6d10831b8feaf7c337f4ab190c662404
Barbe-Bleue <screy@icloud.com>
2018-07-04 11:15:55 +0200

    now you can edit comment

--- cryptoconseils-frontend/src/Profil/Activites.js
+++ cryptoconseils-frontend/src/Profil/Activites.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import axios from 'axios'
+import { action } from '@storybook/addon-actions'
 import Confirm from 'react-confirm-bootstrap';
+import Dialog from 'react-bootstrap-dialog'
 
 class Activites extends Component {
 
@@ -8,8 +10,7 @@ class Activites extends Component {
     super();
     this.state = {
       comments: [],
-      article: [],
-      msg: null
+      msg: null,
     }
   }
   componentDidMount(){
@@ -19,9 +20,11 @@ class Activites extends Component {
         headers: {'Authorization': "Bearer " + sessionStorage.getItem('access_token')}
       };
     }
+
+    // get tous les coms, le "/1" est nécéssaire pour l'api,
+    // la récupération de l'utilisateur courant se fait dans l'api
     axios.get(process.env.REACT_APP_API_ADDRESS+'/users/comments/id/1', authorization)
     .then(response => {
-      console.log(response.data);
       this.setState({
         comments: response.data
       })
@@ -40,19 +43,35 @@ class Activites extends Component {
     var minutes = date.getMinutes();
     return 'Le '+day+' '+month+', '+year+' à '+hours+':'+minutes;
   }
-  getAllArticlesByid(id) {
-    // check si access token
-    if(sessionStorage.getItem('access_token')){
-      var config = {
-        headers: {'Authorization': "Bearer " + sessionStorage.getItem('access_token')}
-      };
-    }
-     axios.get(process.env.REACT_APP_API_ADDRESS+'/articles/'+id, config)
-    .then(response => {
-      return response.data;
-    }).catch(error => {
-      console.log(error);
-    });
+
+  editComment (item) {
+    this.dialog.show({
+      body: 'Nouveau commentaire',
+      prompt: Dialog.TextPrompt({placeholder: 'name', initialValue: item.content}),
+      actions: [
+        Dialog.CancelAction(),
+        Dialog.OKAction((dialog) => {
+          const result = dialog.value
+
+          //check si access token
+          if(sessionStorage.getItem('access_token')){
+            var authorization = {
+              headers: {'Authorization': "Bearer " + sessionStorage.getItem('access_token')}
+            };
+            // update coms
+            axios.put(process.env.REACT_APP_API_ADDRESS+'/comments/'+item.id,{
+              content:result
+            }, authorization).then(response => {
+              //update de l'affichage des commentaires
+              alert("Votre commenaitre a été modifié !");
+            }).catch(error => {
+              alert("Une erreur s'est produite");
+              console.log(error.response);
+            });
+          }
+        })
+      ]
+    })
   }
 
   deleteComment(item){
@@ -61,13 +80,13 @@ class Activites extends Component {
       var authorization = {
         headers: {'Authorization': "Bearer " + sessionStorage.getItem('access_token')}
       };
-      // send coms
+      // delete coms
       axios.delete(process.env.REACT_APP_API_ADDRESS+'/comments/'+item.id, authorization)
       .then(response => {
         const array = this.state.comments;
-        // index de l'item de la todo list que l'on veut supprimer
+        // index de l'item du com que l'on veut supprimer
         const index = array.indexOf(item);
-        // Supprimer de l'affichage le commentaire
+        // Supprimer de l'affichage du commentaire
         array.splice(index, 1)
         //update de l'affichage des commentaires
         this.setState({
@@ -80,6 +99,7 @@ class Activites extends Component {
       });
     }
   }
+
   renderComments(){
     return this.state.comments.map(item => {
       return(
@@ -90,7 +110,7 @@ class Activites extends Component {
             </div>
             <div className="col-xs-10 col-md-11">
               <div>
-                <a href={"/article/"+item.article_id}>UN TITRE D'ARTICLE</a>
+                <a href={"/articles/"+item.article_id}>{item.title}</a>
                 <div className="mic-info">
                   {this.convertDate(item.date)}
                 </div>
@@ -99,7 +119,7 @@ class Activites extends Component {
                 {item.content}
               </div>
               <div className="action">
-                <button type="button" className="btn btn-info btn-xs button-profil" title="Edit">
+                <button  onClick={this.editComment.bind(this,item)} type="button" className="btn btn-info btn-xs button-profil" title="Edit">
                   <span className="fa fa-pencil"></span>
                 </button>
                 <Confirm
@@ -114,7 +134,9 @@ class Activites extends Component {
                     <span className="fa fa-trash"></span>
                   </button>
               </Confirm>
-
+              <div>
+                <Dialog ref={(el) => { this.dialog = el }} />
+              </div>
               </div>
             </div>
           </div>
@@ -126,8 +148,8 @@ class Activites extends Component {
     return (
       <div className="container bootstrap snippet">
         <div className="row text-center">
-          <h2 className="title-head" id="commentaires">Dernières<span>
-              Activités</span>
+          <h2 className="title-head" id="commentaires">Dernières
+            <span>Activités</span>
           </h2>
           <div className="title-head-subtitle">
             <p>Un rapide coup d'oeil de votre activité sur le site</p>
