6bb4a29ad277404948b2618aacbc675b90ff1cbf
Barbe-Bleue <screy@icloud.com>
2018-07-04 16:52:55 +0200

    remove MenuPremium, diff between premium and no-premium is juste banner

--- cryptoconseils-frontend/src/Profil/Menus.js
+++ cryptoconseils-frontend/src/Profil/Menus.js
@@ -1,42 +1,50 @@
 import React, { Component } from 'react';
-import MenusPremium from './Premium/MenusPremium';
 
 class Menus extends Component {
-  render() {
-    if(this.props.premium >= 2) {
-      return <MenusPremium/>
-    }else {
-      return (
-        <div className="Menus">
-           <div className="menu-dashboard">
-              <div className="container">
-                  <div className="row text-center">
-                      <div className="col-md-3 col-sm-3 col-xs-12">
-                          <a href="#commentaires">
-                              <h3 className="titre-dashboard">Commentaires</h3>
-                          </a>
-                      </div>
-                      <div className="col-md-3 col-sm-3 col-xs-12">
-                          <a href="#call-premium">
-                              <h3 className="titre-dashboard">Call premium</h3>
-                          </a>
-                      </div>
-                      <div className="col-md-3 col-sm-3 col-xs-12">
-                          <a href="#airdrop-premium" className="">
-                              <h3 className="titre-dashboard">Airdrop premium</h3>
-                          </a>
-                      </div>
-                      <div className="col-md-3 col-sm-3 col-xs-12">
-                          <a href="#infos-perso" className="">
-                              <h3 className="titre-dashboard">Infos persos</h3>
-                          </a>
-                      </div>
-                  </div>
-              </div>
-          </div>
+
+  premiumBanner(){
+    if(this.props.premium >= 2){
+      return(
+        <div className="text-center">
+          <img src="images/backoffice/logo-premium.png" className="img-responsive center-block premium-logo"/>
         </div>
       );
+    }else {
+      return null;
     }
+  }
+  render() {
+    return (
+      <div className="Menus">
+         <div className="menu-dashboard">
+            <div className="container">
+              {this.premiumBanner()}
+                <div className="row text-center">
+                    <div className="col-md-3 col-sm-3 col-xs-12">
+                        <a href="#commentaires">
+                            <h3 className="titre-dashboard">Commentaires</h3>
+                        </a>
+                    </div>
+                    <div className="col-md-3 col-sm-3 col-xs-12">
+                        <a href="#call-premium">
+                            <h3 className="titre-dashboard">Call premium</h3>
+                        </a>
+                    </div>
+                    <div className="col-md-3 col-sm-3 col-xs-12">
+                        <a href="#airdrop-premium" className="">
+                            <h3 className="titre-dashboard">Airdrop premium</h3>
+                        </a>
+                    </div>
+                    <div className="col-md-3 col-sm-3 col-xs-12">
+                        <a href="#infos-perso" className="">
+                            <h3 className="titre-dashboard">Infos persos</h3>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+      </div>
+    );
   }
 }
 
