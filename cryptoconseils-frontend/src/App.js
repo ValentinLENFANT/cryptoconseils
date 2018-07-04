272f9ad1280ed964ecba1ea72cb989e654720b96
Barbe-Bleue <screy@icloud.com>
2018-07-04 19:22:02 +0200

    remove useless route

--- cryptoconseils-frontend/src/App.js
+++ cryptoconseils-frontend/src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Loadable from 'react-loadable';
 import './App.css';
-import ProfilPremium from './ProfilPremium/ProfilPremium';
 
 const Loading = () => <div>Loading...</div> ;
 
