f7e7c723f58b5adfc6b27dca63017b9311298e6c
Barbe-Bleue <screy@icloud.com>
2018-07-08 21:28:35 +0200

    add denied return html

--- cryptoconseils-frontend/src/Denied/Denied.js
+++ cryptoconseils-frontend/src/Denied/Denied.js
@@ -23,6 +23,17 @@ class Denied extends Component {
           <h2><a href="/">Retour à l'acceuil</a></h2>
         </div>
       );
+    } else {
+      return (
+        <div className="Denied">
+          <h1>
+            Pas accès
+          </h1>
+          <h2><a href="/signin">Se connecter</a></h2>
+          <h2><a href="/signup">S'inscrire</a></h2>
+          <h2><a href="/">Retour à l'acceuil</a></h2>
+        </div>
+      );
     }
 
   }
