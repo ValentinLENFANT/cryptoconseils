379831e8894f2cf5ace12cd22a48a77cda1507c9
Barbe-Bleue <screy@icloud.com>
2018-07-10 20:26:47 +0200

    fix for build

--- cryptoconseils-frontend/src/Home/JsFunction.js
+++ cryptoconseils-frontend/src/Home/JsFunction.js
@@ -3,7 +3,7 @@ import { Component } from 'react';
 class JsFunction extends Component {
   componentDidMount(){
   const script = document.createElement("script");
-    script.src = "/js/all.js";
+    script.src = "./js/all.js";
     script.async = true;
     document.body.appendChild(script);
   }
