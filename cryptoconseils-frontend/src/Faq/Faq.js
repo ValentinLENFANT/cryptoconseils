b381e2f9185a357d54e59a6a18ff44d8943ea7d5
BERANGER Aurélien <aurelien.beranger@gmail.com>
2018-07-16 15:33:38 +0200

    MAJ de la FACK
    je te push ma faq <3 Amicalement & tendrement

--- cryptoconseils-frontend/src/Faq/Faq.js
+++ cryptoconseils-frontend/src/Faq/Faq.js
@@ -14,18 +14,136 @@ class Faq extends Component {
               <div className="col-xs-12 col-md-12">
                 {/* Panel Group Starts */}
                 <div className="panel-group" id="accordion">
-                  {/* Panel Starts */}
-                  <div className="panel panel-default">
+                 {/* Panel Starts */}
+                 <div className="panel panel-default">
                     {/* Panel Heading Starts */}
                     <div className="panel-heading">
                       <h4 className="panel-title">
                         <a data-toggle="collapse" data-parent="#accordion" href="#collapse1">
-                          Qu'est ce que le Bitcoin ?</a>
+                          Pourquoi Devenir Membre Premium ?</a>
                       </h4>
                     </div>
                     {/* Panel Heading Ends */}
                     {/* Panel Content Starts */}
                     <div id="collapse1" className="panel-collapse collapse in">
+                      <div className="panel-body">Il existe 4 catégories de membre premium : DEBUTANT (niveau 1), AVANCÉ (niveau 2), EXPERT (niveau 3), LAMBO (niveau 4). Ces catégories offrent chacune des fonctionnalités liés au site en fonction du niveau premium de l'utilisateur. Plus l'utilisateur a un niveau premium élevé plus il aura accès à de nouvelles fonctionnalités.
+                    </div>
+                    {/* Panel Content Starts */}
+                  </div>
+                  {/* Panel Starts */}
+                  <div className="panel panel-default">
+                    {/* Panel Heading Starts */}
+                    <div className="panel-heading">
+                      <h4 className="panel-title">
+                      <a className="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapse11">
+                          Le pack PREMIUM Débutant - niveau 1</a>
+                      </h4>
+                    </div>
+                    {/* Panel Heading Ends */}
+                    {/* Panel Content Starts */}
+                    <div id="collapse11" className="panel-collapse collapse">
+                      <div className="panel-body">Le pack DEBUTANT permettra de vous donner accès aux articles de niveaux "Débutant" sur Cryptoconseils.fr + Un acces complet aux Airdrop du jour</div>
+                    </div>
+                    {/* Panel Content Starts */}
+                  </div>
+                  {/* Panel Ends */}
+                  <div className="panel panel-default">
+                    {/* Panel Heading Starts */}
+                    <div className="panel-heading">
+                      <h4 className="panel-title">
+                      <a className="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapse12">
+                          Le pack PREMIUM AVANCEE - niveau 2</a>
+                      </h4>
+                    </div>
+                    {/* Panel Heading Ends */}
+                    {/* Panel Content Starts */}
+                    <div id="collapse12" className="panel-collapse collapse">
+                      <div className="panel-body">Le pack AVANCEE donnera accès aux articles de niveaux Avancé sur Cryptoconseils.fr + Un accès à la messagerie Telegram + Un accès aux Analyses de Cryptoconseils.fr</div>
+                    </div>
+                    {/* Panel Content Starts */}
+                  </div>
+                  {/* Panel Ends */}
+
+                  {/* Panel Ends */}
+                  <div className="panel panel-default">
+                    {/* Panel Heading Starts */}
+                    <div className="panel-heading">
+                      <h4 className="panel-title">
+                      <a className="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapse13">
+                          Le pack PREMIUM EXPERT - niveau 3</a>
+                      </h4>
+                    </div>
+                    {/* Panel Heading Ends */}
+                    {/* Panel Content Starts */}
+                    <div id="collapse13" className="panel-collapse collapse">
+                      <div className="panel-body">Le pack EXPERT donnera accès aux articles de niveaux Expert sur Cryptoconseils.fr + Un accès à l'ensemble des réseaux sociaux dont le groupe Facebook + accès aux Calls de Cryptoconseils.fr<br /></div>
+                    </div>
+                    {/* Panel Content Starts */}
+                  </div>
+                  {/* Panel Ends */}
+
+                  {/* Panel Ends */}
+                  <div className="panel panel-default">
+                    {/* Panel Heading Starts */}
+                    <div className="panel-heading">
+                      <h4 className="panel-title">
+                      <a className="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapse14">
+                          Le pack PREMIUM LAMBO - niveau 4</a>
+                      </h4>
+                    </div>
+                    {/* Panel Heading Ends */}
+                    {/* Panel Content Starts */}
+                    <div id="collapse14" className="panel-collapse collapse">
+                      <div className="panel-body">Le pack LAMBO donnera accès à tous les avantages des packs précédents + suivi personnalisé par nos experts traders qui répondront à toutes vos questions.</div>
+                    </div>
+                    </div>
+                    {/* Panel Content Starts */}
+                  </div>
+                  {/* Panel Ends */}
+
+                 <div className="panel panel-default">
+                    {/* Panel Heading Starts */}
+                    <div className="panel-heading">
+                      <h4 className="panel-title">
+                        <a className="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapse15">
+                          Qu'est-ce qu'un call</a>
+                      </h4>
+                    </div>
+                    {/* Panel Heading Ends */}
+                    {/* Panel Content Starts */}
+                    <div id="collapse15" className="panel-collapse collapse">
+                      <div className="panel-body">Un call est une indication de notre part concernant l'achat d'une cryptomonnaie. Nous détaillons très simplement pourquoi il est intéressant d'acheter maintenant cette cryptomonnaie, son prix d'achat ainsi que le prix de revente estimé. Nos calls premiums sont uniquement accessibles pour les utilisateurs premiums EXPERTS (niveau 3). Vous aurez quelques heures à l'avance nos calls avant publication sur le site.</div>
+                    {/* Panel Content Starts */}
+                    </div>
+                  </div>
+
+                  <div className="panel panel-default">
+                    {/* Panel Heading Starts */}
+                    <div className="panel-heading">
+                      <h4 className="panel-title">
+                        <a className="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapse16">
+                          Qu'est-ce qu'un airdrop premium ?</a>
+                      </h4>
+                    </div>
+                    {/* Panel Heading Ends */}
+                    {/* Panel Content Starts */}
+                    <div id="collapse16" className="panel-collapse collapse">
+                      <div className="panel-body">Un airdrop premium est un renseignement de notre part pour obtenir des cryptomonnaies gratuitements. Nous sélectionnons et auditons les différentes cryptomonnaies disponible en Airdrop pour uniquement vous offrir celles qui ont le meilleurs potentiel d'évolution au cours du temps.</div>
+                    {/* Panel Content Starts */}
+                    </div>
+                  </div>
+
+                  {/* Panel Starts */}
+                  <div className="panel panel-default">
+                    {/* Panel Heading Starts */}
+                    <div className="panel-heading">
+                      <h4 className="panel-title">
+                      <a className="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapse10">
+                          Qu'est ce que le Bitcoin ?</a>
+                      </h4>
+                    </div>
+                    {/* Panel Content Starts */}
+                    <div id="collapse10" className="panel-collapse collapse">
                       <div className="panel-body">Bitcoin est une forme de monnaie numérique basée sur un code source ouvert créé et conservé par voie électronique. Bitcoin est une forme de monnaie décentralisée, ce qui signifie qu'elle n'appartient à aucune forme de gouvernement et n'est contrôlée par personne.</div>
                     </div>
                     {/* Panel Content Starts */}
