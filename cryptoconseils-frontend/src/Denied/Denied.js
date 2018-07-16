a9fcdadaedd74d159cb68b8125e191ad19a5fd91
Barbe-Bleue <screy@icloud.com>
2018-07-16 10:49:58 +0200

    add header to denied

--- cryptoconseils-frontend/src/Denied/Denied.js
+++ cryptoconseils-frontend/src/Denied/Denied.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-
+import Header from '../Header/Header'
 class Denied extends Component {
 
   render() {
     if(this.props.noLogged) {
       return (
         <div className="Denied">
+          <Header/>
           <h1>Vous n'ètes pas connecté</h1>
           <h2><a href="/signin">Se connecter</a></h2>
           <h2><a href="/signup">S'inscrire</a></h2>
@@ -15,6 +16,8 @@ class Denied extends Component {
     } else if(this.props.noAccess){
       return (
         <div className="Denied">
+          <Header/>
+
           <h1>
             Vous n'avez pas le niveau <a href="/premium">premium </a>suffisant
           </h1>
@@ -26,6 +29,8 @@ class Denied extends Component {
     } else {
       return (
         <div className="Denied">
+          <Header/>
+
           <h1>Vous n'avez pas accès à cette page</h1>
           <h2><a href="/signin">Se connecter</a></h2>
           <h2><a href="/signup">S'inscrire</a></h2>
