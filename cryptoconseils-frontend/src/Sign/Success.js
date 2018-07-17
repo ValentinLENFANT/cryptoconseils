35569bc93586661ae5f00387d9afb4708454dc16
Barbe-Bleue <screy@icloud.com>
2018-07-17 02:40:53 +0200

    fix css for 13inch

--- cryptoconseils-frontend/src/Sign/Success.js
+++ cryptoconseils-frontend/src/Sign/Success.js
@@ -7,7 +7,7 @@ class Success extends Component {
 
         <div className="container text-center">
           <div className="row">
-            <div className="col-sm-12 col-md-12 col-xs-12">
+            <div className="col-sm-10 col-md-10 col-xs-10">
               <p>Votre inscription a été prise en compte !</p>
               <p> Un email vous a été envoyé à l'adresse {this.props.email}</p>
               <a href="/">Retour à la page d'accueil</a>
--- cryptoconseils-frontend/src/Success/Success.js
+++ cryptoconseils-frontend/src/Success/Success.js
@@ -5,8 +5,8 @@ class Success extends Component {
     if(this.props.email){
       return (
       <div className="container text-center">
-        <div className="Success Component contact-form">
-          <div className="col-sm-12 col-md-12 col-xs-12 text-center output_message_holder d-block">
+        <div className="Success Component contact-form col-sm-9 col-md-9 col-xs-9 text-center">
+          <div className="output_message_holder d-block">
             <p className="output_message success">Votre inscription a été prise en compte !</p>
           </div>
           <p> Un email vous a été envoyé à l'adresse <b>{this.props.email}</b></p>
@@ -17,9 +17,9 @@ class Success extends Component {
     } else if (this.props.activated) {
       return (
         <div className="container text-center">
-          <div className="Success Component contact-form">
+          <div className="Success Component contact-form col-sm-9 col-md-9 col-xs-9 text-center">
             <meta http-equiv="refresh" content="3; url=/" />
-              <div className="col-sm-12 col-md-12 col-xs-12 text-center output_message_holder d-block">
+              <div className=" output_message_holder d-block">
                 <p className="output_message success">Votre compte a été activé !</p>
               </div>
             <p> Vous allez être redirigé sur la page d'accueil</p>
@@ -30,8 +30,8 @@ class Success extends Component {
     } else if (this.props.forgotPassword) {
       return (
         <div className="container text-center">
-          <div className="Success Component contact-form">
-            <div className="col-sm-12 col-md-12 col-xs-12 text-center output_message_holder d-block">
+          <div className="Success Component contact-form col-sm-9 col-md-9 col-xs-9 text-center">
+            <div className=" output_message_holder d-block">
               <p className="output_message success">L'email de réinitialisation du mot de passe a bien été envoyé</p>
             </div>
             <p> Veuillez cliquer sur le lien envoyé par mail pour procéder à la rénitialisaiton de votre mot de passe</p>
@@ -42,9 +42,9 @@ class Success extends Component {
     } else if (this.props.newPassword) {
       return (
         <div className="container text-center">
-          <div className="Success Component contact-form">
+          <div className="Success Component contact-form col-sm-9 col-md-9 col-xs-9 text-center">
             <div className="row">
-              <div className="col-sm-12 col-md-12 col-xs-12 text-center output_message_holder d-block">
+              <div className="output_message_holder d-block">
                 <p className="output_message success">Votre mot de passe a été changé !</p>
               </div>
             </div>
